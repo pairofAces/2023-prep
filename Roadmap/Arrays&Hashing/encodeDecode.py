4c72b6b574addbfa55e9f6e3791d3ce67607b270
pairofAces <karanchauhan16@gmail.com>
2023-03-02 18:38:36 -0500

    created encode and decode solution, tested

--- Roadmap/Arrays&Hashing/encodeDecode.py
+++ Roadmap/Arrays&Hashing/encodeDecode.py
@@ -19,4 +19,30 @@ class Solution:
             result += str(len(s)) + "#" + s
         return result
 
+    '''
+    @param: s: a string
+    @return decodes a single string to a list of strings
+    '''
     def decode(self, s):
+        result, i = [], 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            result.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return result
+
+solution = Solution()
+
+strs = ["leet","code","loves","you"]
+
+test1 = solution.encode(strs)
+print(test1)
+
+s = "4#leet4#code5#loves3#you"
+
+test2 = solution.decode(s)
+print(test2)
